30609f88ef9a93ef97785b4c2ca051457ae2cc9f
Marcos Barrera <elyomarcos@gmail.com>
2017-07-26 10:14:03 -0500

    First own commit

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+const http = require('http');
+const fs = require('fs');
+const port = 8888;
+
+//404 response.
+function send404Response(res) {
+  res.writeHead(404, {'Content-Type': 'text/plain'});
+  res.write("Error 404: Page not found");
+  res.end();
+}
+
+//Handle user request.
+function onRequest(req, res){
+  if (req.method == 'GET' && req.url == '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream("./index.html").pipe(res);
+
+  }else {
+    send404Response(res);
+  }
+}
+
+
+
+http.createServer(onRequest).listen(port);
+console.log("Server is running...");
